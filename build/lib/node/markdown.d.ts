8728e1811aa46af59160dda5b3b29231b8626b4e
Nathan Kelly <nathan.m.kelly@gmail.com>
2018-12-19 13:38:54 +1100

    remove old develop files

--- build/lib/node/markdown.d.ts
+++
@@ -1,9 +0,0 @@
-import { TwingNode, TwingCompiler, TwingNodeOutputInterface } from "Twing";
-/**
- * Represents a markdown node.
- */
-export declare class TwingNodeMarkdown extends TwingNode {
-    TwingNodeOutputInterfaceImpl: TwingNodeOutputInterface;
-    constructor(body: TwingNode, lineno: number, columnno: number, tag?: string);
-    compile(compiler: TwingCompiler): void;
-}
